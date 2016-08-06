6fc785303b56d7eeb8acfb60e17e980caca5e15c
sanlouise <sandra.adamshallie@gmail.com>
2016-08-06 22:15:51 +0200

    Update initial view and instantiate NavigatorIOS

---
+++ App/Components/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react-native';
+
+const {
+	View,
+	Text
+} = React;
+
+class Main extends Component {
+  render() {
+    return (
+      <View>
+      	<Text>"Testing Router" </Text>
+      </View>
+
+    );
+  }
+}
--- index.ios.js
+++ index.ios.js
@@ -9,24 +9,21 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  NavigatorIOS,
   View
 } from 'react-native';
 
 class AwesomeProject extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+
+      <NavigatorIOS
+        initialRoute={{
+          title: 'Github Notetaker',
+          component: Main
+        }}
+      />
+
     );
   }
 }
