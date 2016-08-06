81296bd4b0e6e419c19c49d2568cde59ffd57fea
sanlouise <sandra.adamshallie@gmail.com>
2016-08-06 22:45:52 +0200

    Update styles index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -13,11 +13,19 @@ import {
   View
 } from 'react-native';
 
+var styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#111111'
+  },
+});
+
 class AwesomeProject extends Component {
   render() {
     return (
 
       <NavigatorIOS
+      style={styles.container}
         initialRoute={{
           title: 'Github Notetaker',
           component: Main
@@ -28,23 +36,5 @@ class AwesomeProject extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
