af6a0d7b147e24f8b9964635ee212ee22090788c
sanlouise <sandra.adamshallie@gmail.com>
2016-08-07 10:36:06 +0200

    Add custom background styling to each Dashboard button

--- App/Components/Dashboard.js
+++ App/Components/Dashboard.js
@@ -26,25 +26,42 @@ var styles = StyleSheet.create({
 
 class Dashboard extends React.Component{
 
+	makeBackground(btn){
+		var obj = {
+			flexDirection: 'row',
+			alignSelf: 'stretch',
+			justifyContent: 'center',
+			flex: 1
+		}
+		if(btn === 0){
+			obj.backgroundColor = '#48BBEC';
+		} else if (btn === 1){
+			obj.backgroundColor = '#E77AAE';
+		} else {
+			obj.backgroundColor = '#758Bf4';
+		}
+		return obj;
+	}
+
 render(){
   return (
       <View style={styles.container}>
       	<Image source={{uri: this.props.userInfo.avatar_url}} style={styles.image} />
         <TouchableHighlight
+        	style={this.makeBackground(0)}
         	onPress={this.goToProfile.bind(this)}
-        	underlayColor='#88D4F5'>
         	<Text style={styles.buttonText}> View Profile </Text>
         </TouchableHighlight>
 
         <TouchableHighlight
+        	style={this.makeBackground(1)}
         	onPress={this.goToRepos.bind(this)}
-        	underlayColor='#88D4F5'>
         	<Text style={styles.buttonText}> View Repositories </Text>
         </TouchableHighlight>
 
         <TouchableHighlight
+        	style={this.makeBackground(2)}
         	onPress={this.goToNotes.bind(this)}
-        	underlayColor='#88D4F5'>
         	<Text style={styles.buttonText}> View Notes </Text>
         </TouchableHighlight>
       </View>
