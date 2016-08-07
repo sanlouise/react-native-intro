3da86e3f0046eef54d40f492b743b41a50555c24
sanlouise <sandra.adamshallie@gmail.com>
2016-08-07 15:31:56 +0200

    Minor UI fix in Repositories Component

--- App/Components/Repositories.js
+++ App/Components/Repositories.js
@@ -45,16 +45,17 @@ class Repositories extends React.Component{
     var desc = repos[index].description ? <Text style={styles.description}> {repos[index].description} </Text> : <View />;
     return (
       <View key={index}>
-      <View style={styles.rowContainer}>
-      <TouchableHighlight
-      onPress={this.openPage.bind(this, repos[index].html_url)}
-      underlayColor='transparent'>
-      <Text style={styles.name}>{repos[index].name}</Text>
-      </TouchableHighlight>
-      <Text style={styles.stars}> Stars: {repos[index].stargazers_count} </Text>
-      {desc}
-      </View>
-      <Separator />
+        <View style={styles.rowContainer}>
+          <TouchableHighlight
+            onPress={this.openPage.bind(this, repos[index].html_url)}
+            underlayColor='transparent'>
+              <Text style={styles.name}>{repos[index].name}</Text>
+          </TouchableHighlight>
+          <Text style={styles.stars}> Stars: {repos[index].stargazers_count} </Text>
+            {desc}
+            <Separator />
+        </View>
+      
       </View>
       )
     });
