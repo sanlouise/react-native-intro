2a8a5758bcea441f5a373a03bec1dbaba532ae89
sanlouise <sandra.adamshallie@gmail.com>
2016-08-07 13:05:33 +0200

    Add Seperator helper

---
+++ App/Components/Helpers/Seperator.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {
+  View,
+  StyleSheet
+} from 'react-native';
+
+var styles = StyleSheet.create({
+	seperator: {
+		height: 1,
+		backgroundColor: '#E4E4E4',
+		flex: 1,
+		marginLeft: 15
+	}
+})
+
+class Seperator extends React.Component{
+	render() {
+		return (
+			<View style={styles.seperator} />
+		)
+	}
+};
+
+module.exports = Seperator;
